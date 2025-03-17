be87b1280255cb03c00511fdbb5e5f9f25cf2774
curry30 <2109049433@qq.com>
2025-03-17 19:27:26 +0800

    feature:用户短链接分组分表处理

--- admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/com/nageoffer/shortlink/admin/test/UserTableShardingTest.java
@@ -2,26 +2,18 @@ package com.nageoffer.shortlink.admin.test;
 
 public class UserTableShardingTest {
 
-    public static final String SQL = "CREATE TABLE `t_link_%d` (\n" +
+    public static final String SQL = "CREATE TABLE `t_group_%d` (\n" +
             "  `id` bigint(20) unsigned zerofill NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
-            "  `domain` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '域名',\n" +
-            "  `short_uri` varchar(8) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin DEFAULT NULL COMMENT '短链接',\n" +
-            "  `full_short_url` varchar(127) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '完整短链接',\n" +
-            "  `origin_url` varchar(1023) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '原始链接',\n" +
-            "  `click_num` int(11) unsigned zerofill DEFAULT NULL COMMENT '点击量',\n" +
             "  `gid` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分组标识',\n" +
-            "  `favicon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '网络图标',\n" +
-            "  `enable_status` tinyint(1) DEFAULT NULL COMMENT '启用标识:0,未启用标识:1',\n" +
-            "  `creat_type` tinyint(1) DEFAULT NULL COMMENT '创捷类型 0:接口创建，1:控制台创建',\n" +
-            "  `valid_date_type` tinyint(1) DEFAULT NULL COMMENT '有效期类型 0:永久有效，1:自定义',\n" +
-            "  `valid_date` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '有效期',\n" +
-            "  `describe` varchar(1023) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '描述',\n" +
-            "  `create_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '创建时间',\n" +
-            "  `update_time` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',\n" +
+            "  `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '分组名称',\n" +
+            "  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '创建分组用户名',\n" +
+            "  `sort_order` int DEFAULT NULL COMMENT '分组排序',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
             "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识 0:未删除 1:已删除',\n" +
             "  PRIMARY KEY (`id`),\n" +
-            "  UNIQUE KEY `idx_unique_full_short-url` (`full_short_url`) USING BTREE COMMENT '根据域名下唯一添加索引'\n" +
-            ") ENGINE=InnoDB AUTO_INCREMENT=1901305357126938626 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
+            "  UNIQUE KEY `idx_unique_username_gid` (`gid`,`username`) USING BTREE\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1900905571995430916 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;";
 
 
 
