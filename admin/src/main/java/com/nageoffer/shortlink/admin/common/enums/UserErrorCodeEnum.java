5ec144adba5e2e3166520d8ad0011264e3cab138
curry30 <2109049433@qq.com>
2025-03-17 14:23:17 +0800

    feature:开发用户登录验证返回提示信息

--- admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/common/enums/UserErrorCodeEnum.java
@@ -4,13 +4,14 @@ import com.nageoffer.shortlink.admin.common.convention.errorcode.IErrorCode;
 
 public enum UserErrorCodeEnum implements IErrorCode {
 
+    USER_TOKEN_FAIL("A000200","用户Token验证失败"),
+
     USER_NULL("B000200","用户记录不存在"),
 
     USER_USERNAME_NULL("B000201","用戶名已存在") ,
 
     USER_NAME_EXIST("B000202","用户记录已存在"),
     USER_SAVE_ERROR("B000203","用户记录新增失败");
-
 
 
 
