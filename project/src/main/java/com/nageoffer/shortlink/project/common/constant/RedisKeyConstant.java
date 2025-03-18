3102553972d0dc4d0c1daf869e8561d9b7cb56a4
curry30 <2109049433@qq.com>
2025-03-19 00:00:56 +0800

    feature:短链接跳转原始链接

---
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -0,0 +1,19 @@
+package com.nageoffer.shortlink.project.common.constant;
+
+/**
+ * Redis Key 常量类
+ */
+public class RedisKeyConstant {
+
+    /**
+     * 短链接跳转前缀key
+     */
+    public static final String GOTO_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接跳转锁前缀key
+     */
+    public static final String LOCK_GOTO_LINK_KEY = "short-link_lock_goto_%s";
+
+
+}
