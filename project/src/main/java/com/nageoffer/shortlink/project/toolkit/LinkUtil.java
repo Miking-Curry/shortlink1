109ac27d5f30f45490fcd4998cd834355c009db2
curry30 <2109049433@qq.com>
2025-03-22 14:26:24 +0800

    feature:开发短链接监控之基础访问UIP数据

--- project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
+++ project/src/main/java/com/nageoffer/shortlink/project/toolkit/LinkUtil.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.project.toolkit;
 
 import cn.hutool.core.date.DateUnit;
 import cn.hutool.core.date.DateUtil;
+import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Date;
 import java.util.Optional;
@@ -23,5 +24,26 @@ public class LinkUtil {
         return Optional.ofNullable(valiDate)
                 .map(each -> DateUtil.between(new Date(),each, DateUnit.MS))
                 .orElse(DEFAULT_CACHE_VALID_TIME);
+    }
+
+    /**
+     * 获取请求的 IP 地址
+     * @param request 请求
+     * @return 用户真实IP
+     */
+    public static String getIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        return ipAddress;
     }
 }
