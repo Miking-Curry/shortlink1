8ed3f0e1a4512bc2ca5fb12b66f87f74400e7e26
curry30 <2109049433@qq.com>
2025-03-26 18:02:13 +0800

    refactor: 重构短链接监控请求直接存储 MySQL

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -17,12 +17,13 @@ import java.util.List;
 public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
 
     /**
-     * 记录基地区访问监控数据
+     * 记录地区访问监控数据
      */
-    @Insert("INSERT INTO t_link_locale_stats (full_short_url, gid, date, cnt, country, province, city, adcode, create_time, update_time, del_flag )" +
-            "VALUES(#{LinkLocaleStats.fullShortUrl}, #{LinkLocaleStats.gid}, #{LinkLocaleStats.date}, #{LinkLocaleStats.cnt}, #{LinkLocaleStats.country},  #{LinkLocaleStats.province},  #{LinkLocaleStats.city},  #{LinkLocaleStats.adcode}, NOW(), NOW(), 0 )" +
-            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{LinkLocaleStats.cnt};" )
-    void linkLocaleStats(@Param("LinkLocaleStats")LinkLocaleStatsDO linkLocaleStatsDO);
+    @Insert("INSERT INTO " +
+            "t_link_locale_stats (full_short_url, date, cnt, country, province, city, adcode, create_time, update_time, del_flag) " +
+            "VALUES( #{linkLocaleStats.fullShortUrl}, #{linkLocaleStats.date}, #{linkLocaleStats.cnt}, #{linkLocaleStats.country}, #{linkLocaleStats.province}, #{linkLocaleStats.city}, #{linkLocaleStats.adcode}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkLocaleStats.cnt};")
+    void shortLinkLocaleState(@Param("linkLocaleStats") LinkLocaleStatsDO linkLocaleStatsDO);
 
     /**
      * 根据短链接获取指定日期内地区监控数据
@@ -54,6 +55,7 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
             "GROUP BY " +
             "    gid, province;")
     List<LinkLocaleStatsDO> listLocaleByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
+
 
 
 }
