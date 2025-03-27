b59cb664f07b79db93946d2d0988b7be66f822fc
curry30 <2109049433@qq.com>
2025-03-27 08:47:48 +0800

    refactor: 重构短链接系统为微服务方式调用

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UrlTitleController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UrlTitleController.java
@@ -2,7 +2,7 @@ package com.nageoffer.shortlink.admin.controller;
 
 
 import com.nageoffer.shortlink.admin.common.convention.result.Result;
-import com.nageoffer.shortlink.admin.remote.ShortLinkRemoteService;
+import com.nageoffer.shortlink.admin.remote.ShortLinkActualRemoteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
